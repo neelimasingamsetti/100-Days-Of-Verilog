37aa571fb6f4c8171656127c294f9a1d0ae458cb
neelimasingamsetti2005-ops <neelima.singamsetti2005@gmail.com>
2026-05-25 14:56:24 +0530

    added and1_tb.v

---
+++ and1_tb.v
@@ -0,0 +1,19 @@
+module and1_tb;
+  reg a,b;
+  wire y;
+  and1 DUT(.a(a),
+           .b(b),
+           .y(y));
+  initial begin
+    a=0;b=0; #5;
+    $display("a=%b,b=%b,y=%b", a,b,y);
+    a=0;b=1; #5;
+    $display("a=%b,b=%b,y=%b", a,b,y);
+    a=1;b=0; #5;
+    $display("a=%b,b=%b,y=%b", a,b,y);
+    a=1;b=1; #5;
+    $display("a=%b,b=%b,y=%b", a,b,y);
+    $finish;
+  end
+endmodule
+    
