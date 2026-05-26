eabed8f4d4224efd4de73862900ec316ab70d867
neelimasingamsetti2005-ops <neelima.singamsetti2005@gmail.com>
2026-05-26 10:51:56 +0530

    XOR1_tb.v

---
+++ XOR1_tb.v
@@ -0,0 +1,23 @@
+module XOR1_tb;
+  reg a,b;
+  wire y;
+  XOR1 DUT (.a(a),
+            .b(b),
+            .y(y));
+  initial begin
+    a=0;b=0; 
+    #5;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+    a=0;b=1; 
+    #5;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+    a=1;b=0; 
+    #5;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+    a=1;b=1; 
+    #5;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+    $finish;
+  end
+endmodule
+          
