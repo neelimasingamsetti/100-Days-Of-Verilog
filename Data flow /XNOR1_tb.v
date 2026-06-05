1bd27d526f9bda2c89a0d52e76f5d91b0d20b488
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-05 21:31:57 +0530

    Added XNOR1_tb.v

---
+++ Data flow /XNOR1_tb.v
@@ -0,0 +1,17 @@
+module XNOR1_tb;
+  reg a,b;
+  wire y;
+  XNOR1 DUT(.a(a),.b(b),.y(y));
+  initial begin
+    #5 a=0;b=0;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+     #5 a=0;b=1;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+     #5 a=1;b=0;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+     #5 a=1;b=1;
+    #1 $display("a=%b,b=%b,y=%b", a,b,y);
+    $finish;
+  end
+endmodule
+    
