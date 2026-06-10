bfa51ddeb01eb216d1d815ff072831e824e762a1
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-10 12:18:10 +0530

    Added NOT1_tb.v

---
+++ Gate Level/NOT1_tb.v
@@ -0,0 +1,15 @@
+module not1_tb;
+  reg a;
+  wire y;
+  not1 DUT (.a(a),.y(y));
+  initial begin
+    a=0; #5;
+    $display("a=%b,y=%b",a,y);
+    a=1; #5;
+    $display("a=%b,y=%b",a,y);
+    $finish;
+  end
+endmodule
+    
+    
+  
