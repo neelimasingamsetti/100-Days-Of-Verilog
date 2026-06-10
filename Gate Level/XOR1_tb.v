1dee92af6b1f93bbcfd5942bf820775ec9505a9a
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-10 20:04:08 +0530

    Added XOR1_tb.v

---
+++ Gate Level/XOR1_tb.v
@@ -0,0 +1,15 @@
+module xor1_tb;
+  reg a,b;
+  wire y;
+  xor1 UUT(.a(a),.b(b),.y(y));
+  initial begin
+    $monitor("a=%b,b=%b,y=%b",a,b,y);
+    #5 a=0,b=0;
+    #5 a=0,b=1;
+    #5 a=1,b=0;
+    #5 a=1,b=1;
+    $finish;
+  end
+endmodule
+    
+  
