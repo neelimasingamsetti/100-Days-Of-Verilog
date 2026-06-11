dd6aed67a93dbce8d1927c2ebcb8d316dd02e17c
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-11 13:18:21 +0530

    Added HA1_tb.v

---
+++ Gate Level/HA1_tb.v
@@ -0,0 +1,15 @@
+module halfadder1_tb;
+  reg a,b;
+  wire sum,carry;
+  halfadder1 UUT (.a(a),.b(b),.sum(sum),.carry(carry));
+  initial begin
+    $monitor("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+    a=0;b=0; #10;
+    a=0;b=1; #10;
+    a=1;b=0; #10;
+    a=1;b=1; #10;
+    $finish;
+  end
+endmodule
+    
+    
