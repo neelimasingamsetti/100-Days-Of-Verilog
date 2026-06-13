b533763024383042b90372bd77d6f663593bdb7f
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-13 20:26:07 +0530

    Added AND1.v

--- Behavioral/AND1.v
+++ Behavioral/AND1.v
@@ -1,1 +1,13 @@
-
+module and1(a,b,y);
+  input a,b;
+  output reg y;
+  always@(*)
+    begin
+      if(a==1 && b==1)
+        begin
+          y=1;
+        end
+      else
+        y=0;
+    end
+endmodule
