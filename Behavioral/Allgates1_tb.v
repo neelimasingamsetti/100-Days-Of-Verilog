e6f26ef0c21ed5f730e6cdde9985c4c817c4718b
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-15 11:07:27 +0530

    Added Allgates1_tb.v

---
+++ Behavioral/Allgates1_tb.v
@@ -0,0 +1,15 @@
+module allgates1_tb;
+  reg a,b;
+  wire and_y,or_y,not_y,nand_y,nor_y,xor_y,xnor_y;
+  allgates1 DUT (.a(a),.b(b),.and_y(and_y),.or_y(or_y),.not_y(not_y),.nand_y(nand_y),.nor_y(nor_y),.xor_y(xor_y),.xnor_y(xnor_y));
+  initial begin
+    $monitor("a=%b,b=%b,and_y=%b,or_y=%b,not_y=%b,nand_y=%b,nor_=%b,xor_y=%b,xnor_y=%b",a,b,and_y,or_y,not_y,nand_y,nor_y,xor_y,xnor_y);
+    a=0;b=0; #10;
+    a=0;b=1; #10;
+    a=1;b=0; #10;
+    a=1;b=1; #10;
+    $finish;
+  end
+endmodule
+    
+    
