a523842c69d1f1bab7d71a5ef8822d6bdfb02499
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-15 11:23:01 +0530

    Added Half Adder1_tb.v

---
+++ Behavioral/Half Adder1_tb.v
@@ -0,0 +1,16 @@
+module halfadder1_tb;
+reg a,b;
+wire sum,carry;
+  halfadder1 UUT (.a(a),.b(b),.sum(sum),.carry(carry));
+initial begin
+  $monitor("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+  a=0;b=0; #5;
+  a=0;b=1; #5;
+  a=1;b=0; #5;
+  a=1;b=1; #5;
+  $finish;
+end
+endmodule
+ 
+  
+  
