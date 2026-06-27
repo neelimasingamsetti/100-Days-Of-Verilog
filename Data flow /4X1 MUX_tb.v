33273f81b72f32d2ac2e31c6c45ae771b49c95ee
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-27 21:35:21 +0530

    Added  4X1 MUX_tb.v

---
+++ Data flow /4X1 MUX_tb.v
@@ -0,0 +1,17 @@
+module mux4x1_tb;
+  reg [3:0] I;
+  reg [1:0] S;
+  wire Y;
+  mux4x1 DUT (.I(I),.S(S),.Y(Y));
+  initial begin
+    $monitor("I=%b,S=%b,Y=%b",I,S,Y);
+    
+    I=4'b0001; S=2'b00; #5;
+    I=4'b0010; S=2'b01; #5;
+    I=4'b0100; S=2'b10; #5;
+    I=4'b1000; S=2'b11; #5;
+    
+    $finish;
+  end
+endmodule
+    
