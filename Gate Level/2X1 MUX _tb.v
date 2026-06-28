d175035082f88ccbdddea18337897d8dfc539cf9
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-28 10:19:24 +0530

    Added 2X1 MUX _tb.v

---
+++ Gate Level/2X1 MUX _tb.v
@@ -0,0 +1,13 @@
+module mux2x1_tb;
+  reg [1:0] I;
+  reg S;
+  wire Y;
+  mux2x1 DUT (.I(I),.S(S),.Y(Y));
+  initial begin
+    $monitor("I=%b,S=%b,Y=%b",I,S,Y);
+    I=2'b01; S=1'b0; #10;
+    I=2'b10; S=1'b1; #10;
+    $finish;
+  end
+endmodule
+    
