02f08df75ea36d6de7a483caba86eba39ff7cc81
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-28 09:19:05 +0530

    Added 2X1 MUX_tb.v

---
+++ Data flow /2X1 MUX_tb.v
@@ -0,0 +1,12 @@
+module mux2x1_tb;
+  reg [1:0] I;
+  reg S;
+  wire Y;
+  mux2x1 UUT (.I(I),.S(S),.Y(Y));
+  initial begin
+    $monitor("I=%b,S=%b,Y=%b",I,S,Y);
+    I=2'b01; S=1'b0; #5;
+    I=2'b10; S=1'b1; #5;
+    $finish;
+  end
+endmodule
