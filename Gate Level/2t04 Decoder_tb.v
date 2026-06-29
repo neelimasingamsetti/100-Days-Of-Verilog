8f5babf8ddc664ad577dc4f862a0593e4b76607f
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-06-29 15:35:02 +0530

    Added 2t04 Decoder_tb.v

---
+++ Gate Level/2t04 Decoder_tb.v
@@ -0,0 +1,17 @@
+module Dec2_4tb;
+  reg a,b;
+  wire y0,y1,y2,y3;
+  Dec2_4 DUT (.a(a),.b(b),.y0(y0),.y1(y1),.y2(y2),.y3(y3));
+  initial begin
+    a=0;b=0; #5;
+    $display("a=%b,b=%b,y0=%b,y1=%b,y2=%b,y3=%b",a,b,y0,y1,y2,y3);
+    a=0;b=1; #5;
+    $display("a=%b,b=%b,y0=%b,y1=%b,y2=%b,y3=%b",a,b,y0,y1,y2,y3);
+    a=1;b=0; #5;
+    $display("a=%b,b=%b,y0=%b,y1=%b,y2=%b,y3=%b",a,b,y0,y1,y2,y3);
+    a=1;b=1; #5;
+    $display("a=%b,b=%b,y0=%b,y1=%b,y2=%b,y3=%b",a,b,y0,y1,y2,y3);
+    $finish;
+  end
+endmodule
+    
