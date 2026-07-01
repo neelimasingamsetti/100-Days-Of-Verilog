94bf034df56760c6a6b5d900d1bff75a523b254b
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-07-01 14:18:39 +0530

    Added16X1 MUX.v

--- Behavioral/16X1 MUX.v
+++ Behavioral/16X1 MUX.v
@@ -1,29 +1,32 @@
-module mux16x1(I,S,y);
+module mux16x1(I,S,Y);
   input [15:0] I;
   input [3:0] S;
-  output reg y;
+  output reg Y;
   always@(*)begin
     case(S)
       
-      4'b0000:y=I[0];
-      4'b0001:y=I[1];
-      4'b0010:y=I[2];
-      4'b0011:y=I[3];
-      4'b0100:y=I[4];
-      4'b0101:y=I[5];
-      4'b0110:y=I[6];
-      4'b0111:y=I[7];
-      4'b1000:y=I[8];
-      4'b1001:y=I[9];
-      4'b1010:y=I[10];
-      4'b1011:y=I[11];
-      4'b1100:y=I[12];
-      4'b1101:y=I[13];
-      4'b1110:y=I[14];
-      4'b1111:y=I[15];
-      default:y=1'b0;
-      endcase
+      4'b0000: Y=I[0];
+      4'b0001: Y=I[1]; 
+      4'b0010: Y=I[2];  
+      4'b0011: Y=I[3];  
+      4'b0100: Y=I[4];   
+      4'b0101: Y=I[5];
+      4'b0110: Y=I[6]; 
+      4'b0111: Y=I[7];  
+      4'b1000: Y=I[8];
+      4'b1001: Y=I[9];  
+      4'b1010: Y=I[10];  
+      4'b1011: Y=I[11];  
+      4'b1100: Y=I[12]; 
+      4'b1101: Y=I[13]; 
+      4'b1110: Y=I[14];
+      4'b1111: Y=I[15];  
+      default: Y=1'b0;
 
-      end
-      endmodule
-      
+    endcase
+  end
+    endmodule
+       
+
+
+  
