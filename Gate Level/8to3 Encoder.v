e2a87062864c1b5033135e0462beb2b43df9ccf1
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-07-04 18:18:45 +0530

    Added 8to3 Encoder.v

---
+++ Gate Level/8to3 Encoder.v
@@ -0,0 +1,10 @@
+module Enc8_3(Y,a2,a1,a0);
+  input [7:0] Y;
+  output a2,a1,a0;
+
+  or G1 (a0,Y[1],Y[3],Y[5],Y[7]);
+  or G2 (a1,Y[2],Y[3],Y[6],Y[7]);
+  or G3 (a2,Y[4],Y[5],Y[6],Y[7]);
+
+endmodule
+  
