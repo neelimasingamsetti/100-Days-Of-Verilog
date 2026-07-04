ba783ee5721e46aee6d1a14914e8de4753f7e54a
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-07-04 09:30:30 +0530

    Added 4to2 Encoder.v

--- Data flow /4to2 Encoder.v
+++ Data flow /4to2 Encoder.v
@@ -1,17 +1,8 @@
 module Enc4_2(Y,a1,a0);
   input [3:0] Y;
-  output reg a1,a0;
-  always@(*)begin
-    case(Y)
-
-      4'b0001: {a1,a0}=2'b00;
-      4'b0010: {a1,a0}=2'b01;
-      4'b0100: {a1,a0}=2'b10;
-      4'b1000: {a1,a0}=2'b11;
-      default: {a1,a0}=2'b00;
-
-    endcase
-  end
+  output a1,a0;
+  assign a1 = Y[2] | Y[3];
+  assign a0 = Y[1] | Y[3];
 endmodule
       
   
