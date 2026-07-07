d4bd64fdaa5554812f6e8340ef37dba1028a9bc2
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-07-07 20:29:19 +0530

    Added 1-bit Comparator_tb.v

---
+++ Behavioral/1-bit Comparator_tb.v
@@ -0,0 +1,17 @@
+module comp_tb;
+  reg a,b;
+  wire x,y,z;
+  comp UUT (.a(a),.b(b),.x(x),.y(y),.z(z));
+  initial begin
+    $monitor("a=%b,b=%b,x=%b,y=%b,z=%b",a,b,x,y,z);
+    
+    a=0;b=0; #5;
+    a=0;b=1; #5;
+    a=1;b=0; #5;
+    a=1;b=1; #5;
+
+    $finish;
+  end
+endmodule
+    
+    
