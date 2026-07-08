7946d4c448de79df291f9f18bb6531378513f1b3
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-07-08 11:17:27 +0530

    Added 8-bit Comparator_tb.v

---
+++ Behavioral/8-bit Comparator_tb.v
@@ -0,0 +1,14 @@
+module comp_tb;
+  reg [7:0] a,b;
+  wire x,y,z;
+  integer i,j;
+  comp DUT (.a(a),.b(b),.x(x),.y(y),.z(z));
+  initial begin
+    $monitor("a=%b,b=%b,x=%b,y=%b,z=%b",a,b,x,y,z);
+    for(i=0;i<64;i=i+1)
+      for(j=0;j<64;j=j+1)begin
+        a=i;b=j; #5;
+      end
+    $finish;
+  end
+endmodule
