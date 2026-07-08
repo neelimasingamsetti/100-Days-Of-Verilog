ddbd3e38c2255339aa0e01e745d513235bccbaa5
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-07-08 10:34:25 +0530

    Added 2-bit Comparator.v

---
+++ Behavioral/2-bit Comparator.v
@@ -0,0 +1,23 @@
+module comp(a,b,x,y,z);
+  input [1:0] a,b;
+  output reg x,y,z;
+  always@(*)begin
+    if(a>b) begin
+      x=1;
+      y=0;
+      z=0;
+    end
+    else if (a==b)begin
+      x=0;
+      y=1;
+      z=0;
+    end
+    else begin
+      x=0;
+      y=0;
+      z=1;
+    end
+
+  end
+endmodule
+    
