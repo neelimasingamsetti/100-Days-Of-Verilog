bb659ea7a86b7e40ce47e34d19af04e4590c487f
Singamsetti Neelima <neelima.singamsetti2005@gmail.com>
2026-07-08 11:05:33 +0530

    Added 4-bit Comparator_tb.v

---
+++ Behavioral/4-bit Comparator_tb.v
@@ -0,0 +1,16 @@
+module comp_tb;
+  reg [2:0] a,b;
+  wire x,y,z;
+  integer i,j;
+  comp DUT (.a(a),.b(b),.x(x),.y(y),.z(z));
+  initial begin
+    $monitor("a=%b,b=%b,x=%b,y=%b,z=%b",a,b,x,y,z);
+    for(i=0;i<8;i=i+1)
+      for(j=0;j<8;j=j+1)begin
+        a=i;b=j; #5;
+      end
+    $finish;
+  end
+endmodule
+    
+  
